6efa14152400cc6b68f7829205c6fda23f99a89f
brianontiri <foughtsentertainment@gmail.com>
2017-07-24 02:35:50 +0300

    added synyax

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
 	has_many :stores_shoes
-    has_many :brands, through: :stores_shoes #creating many to many relationship
+    has_many :brands, through: :stores_shoes #creating many to many relationship.
 
 	#validates the number of characters input by the user.
 	validates(:name, {:presence => true, :length => { :maximum => 10}})
