413fd2cea903728e68ef3301b4ef1557d85f5655
brianontiri <foughtsentertainment@gmail.com>
2017-07-24 02:32:28 +0300

    added colums to be created in the db

--- db/migrate/20170721084111_brands.rb
+++ db/migrate/20170721084111_brands.rb
@@ -2,8 +2,9 @@ class Brands < ActiveRecord::Migration[5.1]
   def change
     create_table(:brands) do |t|
     t.column(:name, :string)
-  
+
     t.timestamps()
   end
   end
 end
+ #prompts which colums to be added in the table
